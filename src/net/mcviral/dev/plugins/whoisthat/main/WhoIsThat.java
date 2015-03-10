249cfd01ed95e0df0c5fe0cb5711636780ab4d13
Matt <Matt@Matt-Desktop-PC>
2015-03-10 03:44:08 +0000

    First commit

---
+++ src/net/mcviral/dev/plugins/whoisthat/main/WhoIsThat.java
@@ -0,0 +1,11 @@
+package net.mcviral.dev.plugins.whoisthat.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WhoIsThat extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+}
---
+++ src/net/mcviral/dev/plugins/whoisthat/util/FileManager.java
@@ -0,0 +1,77 @@
+package net.mcviral.dev.plugins.whoisthat.util;
+
+import java.io.File;
+import java.io.IOException;
+//import java.io.InputStream;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class FileManager {
+
+	private Plugin plugin;
+	private FileConfiguration customConfig = null;
+	private File customConfigFile = null;
+	private String path = null;;
+	private String filename = null;
+	
+	public FileManager(Plugin mplugin, String mpath, String file){
+		plugin = mplugin;
+		path = mpath;
+		filename = file;
+		customConfigFile = new File(plugin.getDataFolder() + "/" + path, filename + ".yml");
+	}
+	
+	//@SuppressWarnings("deprecation")
+	public void reloadYAML(){
+		customConfigFile = new File(plugin.getDataFolder() + "/" + path, filename + ".yml");
+		if (!customConfigFile.exists()){
+			try {
+				customConfigFile.mkdirs();
+				customConfigFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
+		// Look for defaults in the jar
+		//InputStream defConfigStream = plugin.getResource(path + ".yml");
+		//if (defConfigStream != null) {
+			//YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+			//customConfig.setDefaults(defConfig);
+		//}
+	}
+	
+	public FileConfiguration getYAML() {
+	    if (customConfig == null) {
+	        reloadYAML();
+	    }
+	    return customConfig;
+	}
+	
+	public void saveYAML() {
+	    if (customConfig == null || customConfigFile == null) {
+	        return;
+	    }
+	    try {
+	        getYAML().save(customConfigFile);
+	    } catch (IOException ex) {
+	        plugin.getLogger().log(Level.SEVERE, "Could not save config to " + customConfigFile.getAbsolutePath() + customConfigFile.getName(), ex);
+	    }
+	}
+	
+	public boolean exists(){
+		if (customConfigFile.exists()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void delete(){
+		customConfigFile.delete();
+	}
+	
+}
